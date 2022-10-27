5aa1739a4aa1558898b942c41ab516c18deafebe
tatianas8090 <tatianasolorzanoa@gmail.com>
2022-10-27 17:55:12 -0500

    first commit

---
+++ ISLA DEL TESORO-1.py
@@ -0,0 +1,44 @@
+# Bienvenidos:
+print("Bienvenidos a la isla, tu mision sera encontrar el tesoro")
+print("Te encuentras con piratas, decides:")
+opcion_1 = input("A.huir B. Hablar con ellos ")
+if (opcion_1 == "A."):
+  print("caiste por un agujero. GAME OVER")
+else:
+  print("Escoge una opcion: A. Mapas o B. Acertijos ")
+opcion_2 = input("A.Mapas o B.Acertijos ")
+if (opcion_2 == "A."):
+  print("Seguir la ruta, eres devorado por vestias. GAME OVER")
+else:
+  print("resolver el primer acertijo")
+opcion_3 = input("A. Acerto o B. No acerto ")
+if (opcion_3 == "A."):
+  print("Puedes continuar en el siguiente nivel")
+else:
+  print("Ingresaste al triangulo de las bermudas. GAME OVER")
+opcion_4 = input("A.Reclamar el tesoro B. Luchar por uno mas grande ")
+if (opcion_4 == "A."):
+  print("GAME OVER")
+else:
+  print("Bienvenido al juego por segunda vez")
+print("Te encuentras con piratas, decides:")
+opcion_1 = input("A.huir B. Hablar con ellos ")
+if (opcion_1 == "A."):
+  print("caiste por un agujero. GAME OVER")
+else:
+  print("Escoge una opcion: A. Mapas o B. Acertijos ")
+opcion_2 = input("A.Mapas o B.Acertijos ")
+if (opcion_2 == "A."):
+  print("Seguir la ruta, eres devorado por vestias. GAME OVER")
+else:
+  print("resolver el primer acertijo")
+opcion_3 = input("A. Acerto o B. No acerto ")
+if (opcion_3 == "A."):
+  print("Puedes continuar en el siguiente nivel")
+else:
+  print("Ingresaste al triangulo de las bermudas. GAME OVER")
+opcion_4 = input("A.Reclamar el tesoro B. Luchar por uno mas grande ")
+if (opcion_4 == "A."):
+  print("GAME OVER")
+else:
+  print("Bienvenido al juego por segunda vez")
